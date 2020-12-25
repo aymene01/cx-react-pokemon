c77199e250f94c4eb51b57085ffb781b0bad8e95
Mehdi-githu <mehdi.jabbour@efrei.net>
2020-12-25 16:15:27 +0100

    en tete

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ const app = express()
 const arg = process.argv
 const pokemons = require('./routes/pokemon')
 const morgan = require('morgan')
-
+app.use(cors())
 app.use(express.json())
 app.use('/pokemon', pokemons)
-app.use(cors())
+
 app.use(morgan('tiny'))
 
 
