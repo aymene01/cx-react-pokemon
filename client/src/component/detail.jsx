ae554c82e958c3c41cee3a79df485dfa799ebb6a
Mehdi-githu <mehdi.jabbour@efrei.net>
2020-12-28 19:52:16 +0100

    detail

--- client/src/component/detail.jsx
+++ client/src/component/detail.jsx
@@ -18,21 +18,28 @@ function PokemonDetail({match}) {
     return (
         <div>
             <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemon.ndex}.png`} alt=""/>
-            <h2>Nom</h2>
+            <h2>Identité</h2>
             <ul>
-                <li>nom: {pokemon.nom}</li>
-                <li>nomde: {pokemon.nomde}</li>
-                <li>nomja: {pokemon.nomja}</li>
-                <li>nomromaji: {pokemon.nomromaji}</li>
-                <li>nomtm: {pokemon.nomtm}</li>
+                <li>Couleur: {pokemon.couleur}</li>
+                <li>Espece: {pokemon.espece}</li>
+                <li>Type1: {pokemon.type1}</li>
+                <li>Taille: {pokemon.taille}</li>
+                <li>Poids: {pokemon.poids}</li>
+                <li>Forme: {pokemon.forme}</li>
+                <li>NomFR: {pokemon.nom}</li>
+                <li>NomEN: {pokemon.nomen}</li>
+                <li>NomDE: {pokemon.nomde}</li>
+                <li>NomJA: {pokemon.nomja}</li>
+                <li>Nomromaji: {pokemon.nomromaji}</li>
+                <li>NomTM: {pokemon.nomtm}</li>
             </ul>
             <h2>Attaque</h2>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>Niveau: {pokemon.attaques.niveau}</li>
+                <li>Nom:{pokemon.attaques.nom}</li>
+                <li>Puissance:{pokemon.attaques.puissance}</li>
+                <li>Precision:{pokemon.attaques.precision}</li>
+                <li>Pp:{pokemon.attaques.pp}</li>
             </ul>
         </div>
     )
